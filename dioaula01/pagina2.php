ca7c94bc9326e5c561a39e55b33b4a9722133928
Sabrina Colombiano <sabrina.colombiano@yahoo.com>
2022-04-23 00:27:57 -0300

    new pages

---
+++ dioaula01/pagina2.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Minha primeira pagina php</title>
+</head>
+<body>
+<h1>introdução a programação para internet</h1>
+<p>linguagem de marcação  html e css</p>
+linguagem d eprogramação
+javascript
+ajax
+php
+.net
+asp
+java
+ruby
+python
+perl
+c, c++ c#
+<br>
+
+<h2>termos comuns</h2>
+
+404  erro: quando a pagina não exite
+Algoritmo: sequencia de passos
+ALT: texto alternativo
+API: peça de lego para executar alguma função expecifica, ex caixa de login
+bootstrap: framework mais usado
+breakpoints: pontos chaves para diversos dispositivos
+cookies:rastros que o site deixa
+deploy: partes de um programa
+design responsivo/ adaptativo: adequar a qualquer tela
+DPI: porporção de pontos da tela, para a produção de imagens
+Meta tags: tagas htmls do head expecificas
+MVC:arquitetura de separa programa de controle 
+MVP: minimo produto viavel para disponibilizar para mostrar para o cliente
+SSL:camada de suguranaça
+UI:interface do usuario, a cara deo seu sistema, user interface
+UX: experiencia do usuario , user expirience
+WYSIWYG:what you see is what you get, o que voce vê é o que voce imprime
+	<?php
+			for <$i = 0 , $i < 10 ; $i++ ) {
+					print( "Linha número " . $i . "<br />");
+			}
+	?>
+</body>
+</html>
